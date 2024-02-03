e888aef7d37ca60e57787bf4669c15f3b2c7177f
DeffGI <holldeevv@gmail.com>
2024-02-03 14:30:32 +0700

    Init project

---
+++ src/main/java/com/example/cloudsimpluswebapp/CloudSimPlusWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudsimpluswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudSimPlusWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudSimPlusWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cloudsimpluswebapp/CloudSimPlusWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudsimpluswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudSimPlusWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
