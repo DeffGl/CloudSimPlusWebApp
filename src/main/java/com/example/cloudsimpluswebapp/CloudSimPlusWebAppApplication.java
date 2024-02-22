0d9a65298250cd6137cdc128faca687a76628aae
DeffGI <holldeevv@gmail.com>
2024-02-22 23:14:01 +0700

    Added localization file for messages in html

--- src/main/java/com/example/cloudsimpluswebapp/CloudSimPlusWebAppApplication.java
+++ src/main/java/com/example/cloudsimpluswebapp/CloudSimPlusWebAppApplication.java
@@ -2,6 +2,9 @@ package com.example.cloudsimpluswebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
 public class CloudSimPlusWebAppApplication {
@@ -10,4 +13,13 @@ public class CloudSimPlusWebAppApplication {
 		SpringApplication.run(CloudSimPlusWebAppApplication.class, args);
 	}
 
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setAlwaysUseMessageFormat(true);
+		messageSource.setUseCodeAsDefaultMessage(true);
+		messageSource.setDefaultEncoding("UTF-8");
+		messageSource.setBasenames("classpath:messages/messages_ru");
+		return messageSource;
+	}
 }
